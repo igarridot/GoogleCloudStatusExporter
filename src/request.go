5f8e7e49295fc75a08154611ca9ed1a77ed200c7
Iván Garrido Tamarit <garrido.tamarit.ivan@gmail.com>
2021-09-12 00:38:01 +0200

    Rewrite all code in Go

---
+++ src/request.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+type incident struct {
+	IncidentId          string    `json:"id"`
+	IncidentNumber      string    `json:"number"`
+	BeginsAt            string    `json:"begin"`
+	CreatedAt           string    `json:"created"`
+	EndsAt              string    `json:"end,omitempty"`
+	ModifiedAt          string    `json:"modified,omitempty"`
+	ExternalDescription string    `json:"external_desc"`
+	Updates             []update  `json:"updates,omitempty"`
+	MostRecentUpdate    update    `json:"most_recent_update,omitempty"`
+	StatusImpact        string    `json:"status_impact"`
+	Severity            string    `json:"severity"`
+	AffectedProducts    []product `json:"affected_products"`
+	ServiceKey          string    `json:"service_key"`
+	ServiceName         string    `json:"service_name"`
+	URI                 string    `json:"uri"`
+}
+
+type update struct {
+	CreatedAt    string `json:"created"`
+	ModifiedAt   string `json:"modified"`
+	Status       string `json:"text"`
+	UpdatedDate  string `json:"when"`
+	Updatestatus string `json:"status"`
+}
+
+type product struct {
+	Title string `json:"title"`
+	Id    string `json:"id"`
+}
+
+func obtainGcpStatus() ([]incident, error) {
+	var status []incident
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://status.cloud.google.com/incidents.json", nil)
+	if err != nil {
+		return []incident{}, errors.New("request builder has failed")
+	}
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return []incident{}, errors.New("request to GCP Status webpage failed")
+	}
+	defer resp.Body.Close()
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []incident{}, errors.New("cannot read the response body")
+	}
+	err = json.Unmarshal(responseBody, &status)
+	if err != nil {
+		return []incident{}, errors.New("failed to unmarshal response json body")
+	}
+	return status, nil
+}
